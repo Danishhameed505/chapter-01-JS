4c23e6cc7abfad3fdffb22e3ce91c043741971a5
Danishhameed505 <155366241+Danishhameed505@users.noreply.github.com>
2024-02-27 19:57:19 +0500

    Chapter 01 JS

---
+++ app.js
@@ -0,0 +1,19 @@
+// Question No.01 Write a script to greet your website visitor using JS alert box.
+// Answer : alert("welcome");
+
+// Question Node.02 Write a script to display following message on your web page:
+// Answer: alert("Error! Please enter a valid password");
+
+// Question Node.03 Write a script to display following message on your web page: (Hint : Use line break) 
+// Answer : alert("Welcome to JS Land.. \n Happy Coding!");
+
+// Question Node.04 Write a script to display following messages in sequence:
+// Answer: alert("Welcome to Js Land..."); alert("Happy coding \n Prevent this page from creating additional dialogs");
+
+// Question Node.05 Generate the following message through browser’s developer console:
+// Answer console.log ("Hello... I can run JS through my web browser's console")
+
+// Question Node.06 Make use of alerts in your new/existing HTML & CSS project.
+// Answer: 
+
+
